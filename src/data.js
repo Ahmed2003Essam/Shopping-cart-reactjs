be1e09f02d93247a1f9d7e1b48effaacbd8b6bde
Ahmed Essam Topala <63603118+Ahmed2003Essam@users.noreply.github.com>
2021-01-14 15:31:35 +0200

    add: initialCommit

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const data = {
+    products: [
+      {
+      id:'1',
+       name:'Macbook',
+       price:1400,
+       image:'https://cnet3.cbsistatic.com/img/yjrw7VgWV7a95AvK8Ym0Np4bFXY=/1200x675/2017/06/27/13484418-bfd9-41e2-8f2d-9b4afb072da8/apple-macbook-pro-15-inch-2017-14.jpg',
+    }, 
+
+    {
+        id:'2',
+         name:'Ipad',
+         price:6000,
+         image:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-2020-hero-space-wifi-select?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1598915064000',
+      }, 
+
+      {
+        id:'3',
+         name:'Samsung Galaxy',
+         price:2500,
+         image:'https://oneegnews.com/wp-content/uploads/2020/10/Samsung-Galaxy-S21-Ultra-featured-%D8%B3%D8%A7%D9%85%D8%B3%D9%88%D9%86%D8%AC.jpg'
+      }, 
+
+    ],
+};
+
+export  default data;
